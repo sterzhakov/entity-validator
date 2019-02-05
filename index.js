5aa1fe4d4172f09e363c097ccc6c8f62a00fb93f
Igor <sterjakov@icloud.com>
2019-02-05 17:45:10 +0300

    correct file names

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   validate: require('./build/validate'),
   createAttributesByValidator: require('./build/createAttributesByValidator'),
-  translateMatcherErrors: require('./build/translateMatcherErrors'),
-  types: require('./build/types.js.flow'),
+  translateMatchersErrors: require('./build/translateMatchersErrors'),
 };
